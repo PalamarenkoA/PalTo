e4b85c75d9b200c37e15b7e6f669e9812ad2b26d
andrey <bahvalll@gmail.com>
2016-02-09 22:38:30 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.gradle/2.8/taskArtifacts/cache.properties.lock
    #	.gradle/2.8/taskArtifacts/fileHashes.bin
    #	.gradle/2.8/taskArtifacts/fileSnapshots.bin
    #	.gradle/2.8/taskArtifacts/taskArtifacts.bin

--- app/src/main/java/com/geekhub/palto/activity/ChatActivity.java
+++ app/src/main/java/com/geekhub/palto/activity/ChatActivity.java
@@ -2,26 +2,31 @@ package com.geekhub.palto.activity;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import com.geekhub.palto.R;
-import com.geekhub.palto.databinding.ActivityChatListBinding;
+import com.geekhub.palto.databinding.ActivityChatBinding;
 import com.geekhub.palto.viewmodel.ChatListViewModel;
-import com.geekhub.palto.viewmodel.FirstSettingsViewModel;
+import com.geekhub.palto.viewmodel.ChatViewModel;
 
-public class ChatListActivity extends AppCompatActivity {
-    public ActivityChatListBinding binding;
-    ChatListViewModel model;
+
+public class ChatActivity extends AppCompatActivity {
+
+
+   public ActivityChatBinding binding;
+    ChatViewModel model;
     Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
         context = this;
         binding = DataBindingUtil.
-                setContentView(this, R.layout.activity_chat_list);
-        model = new ChatListViewModel(this);
+                setContentView(this, R.layout.activity_chat);
+        model = new ChatViewModel(this);
         binding.setModel(model);
-    }
-}
+
+
+
+}}
--- app/src/main/java/com/geekhub/palto/viewmodel/ChatViewModel.java
+++ app/src/main/java/com/geekhub/palto/viewmodel/ChatViewModel.java
@@ -1,23 +1,19 @@
 package com.geekhub.palto.viewmodel;
 
-import android.content.Intent;
-import android.view.View;
-import android.widget.AdapterView;
-
 import com.geekhub.palto.activity.ChatActivity;
 import com.geekhub.palto.activity.ChatListActivity;
 import com.geekhub.palto.adapter.DialogListAdapter;
 import com.geekhub.palto.object.ItemDialogList;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 /**
- * Created by andrey on 08.02.16.
+ * Created by andrey on 09.02.16.
  */
-public class ChatListViewModel {
-    ChatListActivity activity;
-
-    public ChatListViewModel (final ChatListActivity activity){
+public class ChatViewModel {
+    ChatActivity activity;
+    public ChatViewModel ( ChatActivity activity){
         this.activity = activity;
         ItemDialogList itemDialogList = new ItemDialogList ("http://cs623630.vk.me/v623630022/44bd6/wEJMXO7t_ic.jpg","Romashka","12.12.03","Где героин, РОМА где он???");
         ArrayList<ItemDialogList> itemDialogListArrayList = new ArrayList<>();
@@ -31,15 +27,8 @@ public class ChatListViewModel {
         itemDialogListArrayList.add(itemDialogList);
         itemDialogListArrayList.add(itemDialogList);
         DialogListAdapter dialogListAdapter = new DialogListAdapter(activity,itemDialogListArrayList);
-        activity.binding.dialogList.setAdapter(dialogListAdapter);
-        activity.binding.dialogList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                activity.startActivity(new Intent(activity, ChatActivity.class));
-            }
-        });
-
-
+        activity.binding.chatList.setAdapter(dialogListAdapter);
+        Picasso.with(activity).load("http://cs623630.vk.me/v623630022/44bd6/wEJMXO7t_ic.jpg").into(activity.binding.myIcon);
+        Picasso.with(activity).load("http://cs623630.vk.me/v623630022/44bd6/wEJMXO7t_ic.jpg").into(activity.binding.yourIcon);
     }
 }
